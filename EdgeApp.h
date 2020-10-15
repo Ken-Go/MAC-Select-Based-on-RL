1a50102c1214db8a52f868db8fc62b06205a428f
zjhao <614471870@qq.com>
2020-10-15 20:59:32 +0800

    finish EdgeApp and modify SenseApp

--- EdgeApp.h
+++ EdgeApp.h
@@ -8,35 +8,63 @@
 #include "ns3/simple-wireless-tdma-module.h"
 #include "ns3/traced-callback.h"
 #include "EdgeTag.h"
-
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/ssid.h"
+#include "ns3/config-store-module.h"
+#include "ns3/wifi-module.h"
+#include "TimeHeader.h"
 #include <iostream>
+#include <cmath>
 #include <fstream>
 #include <string>
 #include <cassert>
 #include <vector>
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("EdgeApp");
-class EdgeApp : public OnOffApplication{
+
+class EdgeApp : public Application{
 public:
     static TypeId GetTypeId(void);
     EdgeApp();
     virtual ~EdgeApp();
-    void ReportOnTime();
-    double CalculateLatency();
-    void UpdateMacPro(NodeContainer nodes,NetDeviceContainer devices,int index,double txpDistance);
-    
-    u_int16_t m_rti; //Report time interval;
-    u_int32_t m_avelatency;//Average latency;
-    bool m_usingtdma;// using tdma protocal?
-    bool m_change; // is change?
+    void Setup(Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate);
+    void CalculateRTT(uint64_t startTime,uint64_t endTime);
+    bool GetUsingTdma();
     TracedCallback<Ptr<const Packet>> m_rxTrace;
-
     TracedCallback<Ptr<const Packet>,const Address&,const Address &> m_rxTraceWithAddress;
 private:
-    // virtual void StartApplication(void);
-    // virtual void StopApplication(void);
+    virtual void StartApplication(void);
+    virtual void StopApplication(void);
+    void ScheduleTx (void);
+    void ScheduleRe (void);
+    void SendPacket (void);
+ 
     void HandleRead (Ptr<Socket> socket);
+    void ReportOnTime();
+    void UpdateMacPro(ApplicationContainer apps,int appIndex,Ipv4InterfaceContainer interface,int interIndex,uint16_t port);
+    
+    Ptr<Socket>     m_socket;
+    Address         m_peer;
+    Address         m_local;
+    EventId         m_sendEvent;
+    EventId         m_reEvent;
+    bool            m_running;
+    uint32_t        m_packetsSent;
+    uint32_t        m_packetSize;
+    uint32_t        m_nPackets;
+    DataRate        m_dataRate;
+
+    uint16_t m_rti; //Report time interval;
+    uint64_t m_avelatency;//Average latency;
+    uint32_t m_count; // count of packets
+    uint64_t m_latencyNow;
+    
+    bool m_usingtdma;// using tdma protocal?
+    bool m_change; // is change?
+    bool m_report;
+    uint32_t m_metrxType; // 1:latency 2:throughput 3:latency and throughput
 };
 
 
@@ -49,12 +77,12 @@ NS_OBJECT_ENSURE_REGISTERED(EdgeApp);
 TypeId 
 EdgeApp::GetTypeId(void){
     static TypeId tid = TypeId("ns3::EdgeApp")
-        .SetParent<OnOffApplication>()
+        .SetParent<Application>()
         .SetGroupName("Applications")
         .AddConstructor<EdgeApp>()
         .AddAttribute("ReportTimeIntervalve",
                     "the intervalue of report the infomation ",
-                    UintegerValue(32),
+                    UintegerValue(1),
                     MakeUintegerAccessor(&EdgeApp::m_rti),
                     MakeUintegerChecker<uint16_t > (1,60))
         .AddAttribute("TDMA",
@@ -68,59 +96,159 @@ EdgeApp::GetTypeId(void){
         .AddTraceSource ("RxWithAddresses", "A packet has been received",
                      MakeTraceSourceAccessor (&EdgeApp::m_rxTraceWithAddress),
                      "ns3::Packet::TwoAddressTracedCallback")
+        .AddAttribute("MetricType",
+                    " 1:latency 2:throughput 3:latency and throughput",
+                    UintegerValue(1),
+                    MakeUintegerAccessor(&EdgeApp::m_metrxType),
+                    MakeUintegerChecker<uint32_t > ())
+        .AddAttribute("Local",
+                    "Local Address",
+                    AddressValue(),
+                    MakeAddressAccessor(&EdgeApp::m_local),
+                    MakeAddressChecker())
+        .AddAttribute("DateRate",
+                    "the data rate in on state",
+                    DataRateValue (DataRate("500kb/s")),
+                    MakeDataRateAccessor(&EdgeApp::m_dataRate),
+                    MakeDataRateChecker())
+        .AddAttribute ("Remote", "The address of the destination",
+                    AddressValue (),
+                    MakeAddressAccessor (&EdgeApp::m_peer),
+                    MakeAddressChecker ())  
+        .AddAttribute ("PacketSize", "The size of packets sent in on state",
+                    UintegerValue (512),
+                    MakeUintegerAccessor (&EdgeApp::m_packetSize),
+                    MakeUintegerChecker<uint32_t> (1))
+        .AddAttribute ("Report", "need report to AP?",
+                    BooleanValue (false),
+                    MakeBooleanAccessor (&EdgeApp::m_report),
+                    MakeBooleanChecker())
     ;
     return tid;
 }
 
 EdgeApp::EdgeApp()
-    : m_rti (10),
+    : m_socket(0),
+      m_peer(),
+      m_local(),
+      m_sendEvent(),
+      m_reEvent(),
+      m_running(false),
+      m_packetsSent(0),
+      m_packetSize(0),
+      m_nPackets(0),
+      m_dataRate(0),
+      m_rti (10),
       m_avelatency(0),
+      m_count(0),
+      m_latencyNow(0),
       m_usingtdma(false),
-      m_change (false)
-
+      m_change (false),
+      m_report(false),
+      m_metrxType(1)
 {
-    NS_LOG_FUNCTION(this);
+    // ReportOnTime();
 }
-
 EdgeApp::~EdgeApp(){
-    NS_LOG_FUNCTION(this);
+  
 }
-void EdgeApp::ReportOnTime(){
-
+ void
+ EdgeApp::Setup (Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate)
+ {
+   m_socket = socket;
+   m_peer = address;
+   m_packetSize = packetSize;
+   m_nPackets = nPackets;
+   m_dataRate = dataRate;
+ }
+ 
+void EdgeApp::StartApplication(void){
+    m_running = true;
+    m_packetsSent = 0;
+    if(!m_local.IsInvalid())
+        m_socket->Bind(m_local);
+    else
+        m_socket->Bind();
+    m_socket->Connect (m_peer);
+    std::cout<<"mnb"<<std::endl;
+    if(m_report)
+        ReportOnTime();
+    std::cout<<"mnb"<<std::endl;
+    SendPacket ();
+   
 }
-
-double EdgeApp::CalculateLatency(){
-
-}
-void 
-Node::DoDispose(){
-    NS_LOG_FUNCTION (this);
-    m_deviceAdditionListeners.clear ();
-    m_handlers.clear ();
-    for (std::vector<Ptr<NetDevice> >::iterator i = m_devices.begin ();i != m_devices.end (); i++)
-    {
-       Ptr<NetDevice> device = *i;
-       device->Dispose ();
-       *i = 0;
+void EdgeApp::StopApplication(void){
+    m_running = false;
+    if(m_sendEvent.IsRunning ()){
+        Simulator::Cancel(m_sendEvent);
     }
-    m_devices.clear ();
+    if(m_socket){
+        m_socket->Close();
+    }
 }
-void EdgeApp::UpdateMacPro(OnOffHelper helper,int index){
+void EdgeApp::SendPacket(void){
+    Ptr<Packet> packet = Create<Packet> (m_packetSize);
+    m_socket->Send(packet);
+    std::cout<<"send packet at "<<Simulator::Now().GetSeconds()<<std::endl;
+    if((uint64_t)Simulator::Now().GetSeconds() %  m_rti == 0){
+        SeqTsSizeHeader header;
+        header.SetSeq(m_avelatency);
+        packet->AddHeader(header);
+    }
+    if(++m_packetsSent < m_nPackets){
+        ScheduleTx ();
+    }
+}
+void
+EdgeApp::ScheduleTx(void){
+    if (m_running){
+        Time tNext (Seconds (m_packetSize * 8 / static_cast<double> (m_dataRate.GetBitRate())));
+        m_sendEvent = Simulator::Schedule (tNext,&EdgeApp::SendPacket,this);
+    }
+}
+
+void EdgeApp::ScheduleRe(void){
+    Time tNext (Seconds (m_rti));
+    m_reEvent = Simulator::Schedule(tNext,&EdgeApp::ReportOnTime,this);
+}
+
+void EdgeApp::ReportOnTime(){
+    Ptr<Packet> packet = Create<Packet> (0);
+    m_socket->Send(packet);
+    if((uint32_t)Simulator::Now().GetSeconds() % m_rti == 0 || Simulator::Now().GetSeconds() == 0){
+        // scheduleRe();
+        std::cout<<"the time is "<<Simulator::Now().GetSeconds()<<" s"<<std::endl;
+        ScheduleRe ();
+    }
+}
+
+void EdgeApp::CalculateRTT(uint64_t startTime,uint64_t endTime){
+    m_latencyNow = endTime - startTime;    
+}
+
+
+bool
+EdgeApp::GetUsingTdma(){
+    return m_usingtdma;
+}
+
+void EdgeApp::UpdateMacPro(ApplicationContainer apps,int appIndex,Ipv4InterfaceContainer interface,int interIndex,uint16_t port){
     if(m_change){
-        
         if(m_usingtdma){
-            
-        }else{
-            
+            apps.Get(appIndex)->SetAttribute("Local",AddressValue(InetSocketAddress(interface.GetAddress(interIndex),port)));
             m_usingtdma = false;
+        }else{
+            apps.Get(appIndex)->SetAttribute("Local",AddressValue(InetSocketAddress(interface.GetAddress(interIndex),port)));
+            m_usingtdma = true;
         }
     }
 }
 
 
 
+
 void EdgeApp::HandleRead (Ptr<Socket> socket){
-    NS_LOG_FUNCTION (this << socket);
+    // NS_LOG_FUNCTION (this << socket);
     Ptr<Packet> packet;
     Address from;
     Address localAddress;
@@ -133,13 +261,24 @@ void EdgeApp::HandleRead (Ptr<Socket> socket){
             EdgeTag tag;
             packet->PeekPacketTag(tag);
             if (tag.GetTagValue() == 0) {
-                NS_LOG_INFO("it is a data packet" + tag.GetTagValue());
-            }else{
-                NS_LOG_INFO("it is a control packet" + tag.GetTagValue());
+                // NS_LOG_INFO("it is a data packet" + tag.GetTagValue());
+                //ischange
                 SeqTsSizeHeader seqTs;
                 packet->RemoveHeader(seqTs);
-                uint32_t currentSequenceNumber = seqTs.GetSeq();
-                uint32_t isChange = seqTs.GetSize ();
+                uint32_t firstL = seqTs.GetSeq();
+                std::cout<<firstL<<std::endl;
+                //reciv time
+                TimeHeader header;
+                packet->RemoveHeader(header);
+                Time time(Simulator::Now());
+                CalculateRTT(header.GetData(),time.GetMilliSeconds());
+                m_count++;
+                m_avelatency = m_avelatency + m_latencyNow;
+            }else{
+                // NS_LOG_INFO("it is a control packet" + tag.GetTagValue());
+                SeqTsSizeHeader seqTs;
+                packet->RemoveHeader(seqTs);
+                uint32_t isChange = seqTs.GetSeq();
                 if (isChange > 0)
                     m_change = true;
                 else
--- SenseApp.h
+++ SenseApp.h
@@ -1,13 +1,7 @@
 #include "ns3/core-module.h"
-#include "ns3/point-to-point-module.h"
 #include "ns3/network-module.h"
 #include "ns3/applications-module.h"
-#include "ns3/mobility-module.h"
-#include "ns3/csma-module.h"
-#include "ns3/ipv4-global-routing-helper.h"
 #include "ns3/internet-module.h"
-#include "ns3/yans-wifi-helper.h"
-#include "ns3/ssid.h"
 #include "ns3/traced-callback.h"
 #include "ns3/event-id.h"
 #include "ns3/ptr.h"
@@ -17,16 +11,72 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cassert>
 #include <vector>
 using namespace ns3;
 
-class SenseApp : public UdpServer{
+class SenseApp : public Application{
 public:
+    static TypeId GetTypeId(void);
+    SenseApp();
+    virtual ~SenseApp();
     void SenseEnv();
     void CalculateDelay();
 private:
-    virtual void StartApplication(void);
+    Ptr<Socket>     m_socket;
+    Address         m_peer;
+    Address         m_local;
+    EventId         m_sendEvent;
+    
+    std::vector<uint64_t> m_metrics;    //the metrics of nodes which is belong to this ap node.
+    std::vector<bool> m_usingtdmas;     //all the node is using tdma? 
+    uint32_t m_rti;                     // report time interval
+    uint32_t m_period;                  // the period of update the control info
+    bool     m_report;
+    virtual void StartApplicaition(void);
     virtual void StopApplication(void);
     void HandleRead (Ptr<Socket> socket);
+    void SendMetrics();
+    void Update();
 };
+NS_OBJECT_ENSURE_REGISTERED(SenseApp);
+
+TypeId
+SenseApp::GetTypeId(void){
+    static TypeId tid = TypeId("ns3::SenseApp")
+        .SetParent<Application>()
+        .SetGroupName("Applicaition")
+        .AddConstructor<SenseApp>()
+        // .AddAttribute()
+        .AddAttribute("Local",
+                    "Local Address",
+                    AddressValue(),
+                    MakeAddressAccessor(&SenseApp::m_local),
+                    MakeAddressChecker())
+        .AddAttribute ("Remote", "The address of the destination",
+                    AddressValue (),
+                    MakeAddressAccessor (&SenseApp::m_peer),
+                    MakeAddressChecker ()) 
+    ;
+    return tid;
+}
+
+SenseApp::SenseApp()
+    : m_socket(0),
+      m_peer(),
+      m_local(),
+      m_sendEvent(),
+      m_metrics(),
+      m_usingtdmas(),
+      m_rti(2),
+      m_period(10),
+      m_report(true)
+{
+
+}
+SenseApp::~SenseApp(){
+
+}
+void
+SenseApp::Setup(){
+    
+}
